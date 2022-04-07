9da838c3084383c53368f4ac8a1ce22057a94065
myla steiger <mrs.steiger@gmail.com>
2022-04-07 16:27:09 -0400

    added product

---
+++ prs-client-solution/src/app/prs/product/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  
+  products: Product[] = [];
+
+  constructor(
+    private prdsvc: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.prdsvc.list().subscribe({
+      next: (res) => {
+        this.products = res;
+        console.debug("products", res)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    });
+  }
+
+}
